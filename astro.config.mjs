14215c1a6847b95a16731d014a96b05b2b66c353
Tomoya Eguchi <mail@cumulo.works>
2024-12-07 03:02:56 +0900

    init: ランディングページを追加。Astro初期設定。

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,30 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	prefetch: {
+		prefetchAll: true,
+	},
+	integrations: [
+		tailwind(),
+		icon({
+			"fa6-solid": ["*"],
+			"fa6-brands": ["*"],
+		}),
+		sitemap(),
+	],
+	image: {
+		service: {
+			entrypoint: "astro/assets/services/sharp",
+		},
+	},
+	output: "static",
+	site: "https://kumo.productions",
+	markdown: {
+		shikiConfig: {
+			theme: "github-light",
+		},
+	},
+});
---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		fontFamily: {
+			sans: ["Inter", `"M PLUS 1p"`, "sans-serif"],
+		},
+		extend: {},
+	},
+	plugins: [],
+};
